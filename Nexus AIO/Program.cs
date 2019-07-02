582b286d816bb0993106a9d023bae428bbce17d0
nexusaio <nexuscore@outlook.com.br>
2019-07-02 15:31:03 -0300

    Versão 0.1

---
+++ Nexus AIO/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using EnsoulSharp;
+using EnsoulSharp.SDK;
+using Nexus_AIO.Champions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus_AIO
+{
+    
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += On_LoadGame;
+        }
+        private static void On_LoadGame()
+        {
+
+            if (ObjectManager.Player.CharacterName == ObjectManager.Player.CharacterName)
+            {
+                Morgana.OnLoad();
+                Chat.PrintChat("NEXUS_AIO LOADED, GGWP!");
+            }
+        }
+    }
+}
